14268a243c8cf5194ff01783b1e43331db394363
unknown <muhammedsadeed70@gmail.com>
2022-07-02 15:07:56 +0400

    scroll to top

--- football/src/App.js
+++ football/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,6 +8,9 @@ import Button from "./components/Button";
 import Transfer from "./components/Transfer";
 
 function App() {
+  useEffect(() =>{
+    window.scrollTo(0,0)
+  }, [])
   return (
     <>
     <Navigation />
