ba66a3735f85e522ca4991b14530451e2808e18f
unknown <muhammedsadeed70@gmail.com>
2022-07-03 19:35:44 +0400

    Final commit

--- football/src/components/Transfer.js
+++ football/src/components/Transfer.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
+import {Modal, Button} from 'react-bootstrap'
 
 function Transfer() {
 
-  const [state, setstate] = React.useState();
+  const [input, setInput] = React.useState('Chelsea');
+
   const [name, setname] = React.useState('Chelsea');
+  const [id, setid] = React.useState(49);
+  const [state, setstate] = React.useState();
 
   useEffect(() =>{
     axios({
@@ -15,11 +19,24 @@ function Transfer() {
         'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
       }
     }).then(res =>{
-      console.log(res.data)
-      setstate(res.data.response[0].team)
+      setid(res.data.response[0].team.id)
     })
 
   }, [name])
+
+  useEffect(() =>{
+    axios({
+      method: 'GET',
+      url: `https://v3.football.api-sports.io/players/?team=${id}&season=${new Date().getFullYear()}`,
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_FOOTBALL_API_KEY,
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+      }
+    }).then(res =>{
+      console.log(res.data.response)
+      setstate(res.data.response)
+    })
+  }, [id])
 
   return (
     <>
@@ -44,20 +61,35 @@ function Transfer() {
         opacity: '0.8',
       }}>Enter a team name</p>
 
-      <input placeholder='Eg: Chelsea' type={'text'} autoComplete='off' value={name} onChange={(e) =>{
-        setname(e.target.value)
+      <div className='d-flex'><input placeholder='Eg: Chelsea' type={'text'} autoComplete='off' value={input} onChange={(e) =>{
+        setInput(e.target.value)
       }} style={{
         padding:'10px',
-        borderRadius:'7px',
         outline:'none',
         border:'none',
         boxShadow: '2px 1px 36px -8px rgba(0,0,0,0.51)',
         WebkitBoxShadow: '2px 1px 36px -8px rgba(0,0,0,0.51)',
         MozBoxShadow: '2px 1px 36px -8px rgba(0,0,0,0.51)',
-        width:'min(60%, 18em)'
-      }}/>
+        width:'min(90%, 55em)'
+      }}/><Button variant='primary' onClick={() =>{
+        setname(input)
+      }} >Submit</Button></div>
 
-      <Player />
+      <div style={{
+        margin:'1.6em 1em 0em 1em',
+        paddingTop:'1.6em',
+      }}><p style={{
+        opacity: '0.8',
+        backgroundColor:'#ccecff',
+        textAlign:'center',
+        padding:'8px',
+        borderLeft:'4px solid blue',
+        borderRadius:'6px',
+      }}>Click on the players to see their transfer status</p></div>
+
+      {state && state.map((player, index) =>{
+        return <Player key={index} id={player.player.id} name={player.player.name} img={player.player.photo} age={player.player.age} nationality={player.player.nationality}/>
+      })}
     </div>
     </>
   )
@@ -66,19 +98,92 @@ function Transfer() {
 export default Transfer
 
 function Player(props){
+
+  const [modalShow, setModalShow] = React.useState(false);
+
   return(
-    <div style={{
-      display:'grid',
-      placeItems:'center',
-      marginTop:'2.2em'
-    }}>
-      <ul style={{
-        listStyle:'none',
-        padding:'5px',
+
+      <div onClick={() =>{
+        setModalShow(true);
+      }} style={{
+        borderRadius:'7px',
+        padding:'1em',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'space-between',
+        marginTop:'1.2em',
+        WebkitBoxXhadow: '4px 10px 26px -11px rgba(0,0,0,0.65)',
+        MozBoxShadow: '4px 10px 26px -11px rgba(0,0,0,0.65)',
+        boxShadow:' 4px 10px 26px -11px rgba(0,0,0,0.65)',
+
       }}>
-        <li>1</li>
-        <li>2</li>
-      </ul>      
-    </div>
+
+        <img src={props.img} alt='Player photo' height={40} width={40}/>
+        <div className='ps-5 ms-5 d-flex align-items-center justify-content-center'><p style={{
+          verticalAlign:'middle',
+          alignSelf:'center',
+          opcaity:'0.8',
+        }}>{props.name}</p></div>
+  <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        name={props.name}
+        age={props.age}
+        nationality={props.nationality}
+        id={props.id}
+      />
+
+      </div>
+
   )
+}
+
+function MyVerticallyCenteredModal(props) {
+  const [transfer, settransfer] = React.useState();
+
+    useEffect(() =>{
+    axios({
+      method: 'GET',
+      url: `https://v3.football.api-sports.io/transfers/?player=${props.id}`,
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_FOOTBALL_API_KEY,
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+      }
+    }).then(res =>{
+      console.log(res.data.response[0])
+      // settransfer(res.data.response[0].transfers[0])
+    })
+  }, [props.id])
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.onHide}
+      
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.name}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Latest transfer</h4>
+        <p>
+          <span className='p-3 fw-bold'>Name: {' '}{props.name}</span><br />
+          <span className='p-3 fw-bold'>Age: {' '}{props.age}</span><br />
+          <span className='p-3 fw-bold'>Nationality: {' '}{props.nationality}</span><br /> 
+          {transfer && ( <>
+            <span className='text-danger'>OUT ⬇️:{transfer.teams.out.name} </span>
+            <span className='text-primary'>IN ⬆️: {transfer.teams.in.name}</span>
+            <span className='text-secondary'>type: {transfer.type}</span> 
+
+          </>)}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+      </Modal.Footer>
+    </Modal>
+  );
 }
